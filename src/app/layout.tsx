1dec162ff93ac305f5f3e866a0b0d1a442f37e82
dpqlsn <a01037111209@gmail.com>
2025-09-02 17:57:49 +0900

    feat(#1) :: 레이아웃 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import '@/styles/GlobalStyle';
+import styled from '@emotion/styled';
 
 export default function RootLayout({
   children,
@@ -8,8 +11,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Container>
+          {children}
+        </Container>
       </body>
     </html>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  height: 100vh;
+  max-height: 100vh;
+  padding: 0.5rem 0.2rem;
+  margin: 0 auto;
+  row-gap: 4rem;
+  background-color: #999;
+
+  @media (min-width: 1024px) {
+    max-width: 600px;
+  }
+
+  @media (max-width: 600px) {
+    max-width: 100vw;
+  }
+`;
---
+++ src/containers/ui/landing/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    row-gap: 1.5rem;
+    width: 100%;
+    max-width: 400px;
+`;
