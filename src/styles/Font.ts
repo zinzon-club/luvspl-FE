ad30030b9256f44cff232f481abfa18ddb0e3c6d
dpqlsn <a01037111209@gmail.com>
2025-09-12 20:58:09 +0900

    add(#3) :: 어비 푸딩체 추가

---
+++ src/styles/Font.ts
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+export const ABeePudding = css`
+  @font-face {
+    font-family: 'ABeePudding';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_five@.2.0/UhBeepuding.woff') format('woff');
+    font-weight: normal;
+    font-display: swap;
+  }
+
+  font-family: 'ABeePudding', sans-serif;
+`;
