2720f7280b089c6e241b3d7d43fcbdc71f491989
dpqlsn <a01037111209@gmail.com>
2025-09-12 20:58:26 +0900

    feat(#3) :: 랜딩 페이지 퍼블리싱

--- src/containers/ui/landing/landing.tsx
+++ src/containers/ui/landing/landing.tsx
@@ -3,7 +3,14 @@ import * as _ from './style';
 export default function Landing() {
   return (
     <_.Container>
-        Landing
+      <_.Group>
+        <_.Box />
+        <_.TextGroup>
+          <_.SubTitle>가장 좋아하는 나에게 주는 선물</_.SubTitle>
+          <_.Title>사랑의 주문</_.Title>
+        </_.TextGroup>
+      </_.Group>
+      <_.Login>화면 클릭 시 카카오톡 로그인 </_.Login>
     </_.Container>
   );
 }
--- src/containers/ui/landing/style.ts
+++ src/containers/ui/landing/style.ts
@@ -1,10 +1,52 @@
 import styled from "@emotion/styled";
+import { ABeePudding } from "@/styles/Font";
+
+export const Basic = `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    row-gap: 1.5rem;
-    width: 100%;
-    max-width: 400px;
+  ${Basic}
+  width: 100%;
+  max-width: 400px;
+  height: 100vh;
+  row-gap: 10rem;
+`;
+
+export const Group = styled.div`
+  ${Basic}
+  height: 30vh;
+  row-gap: 3rem;
+`;
+
+export const Box = styled.div`
+  width: 50%;
+  height: 12vh;
+  background-color: #829671;
+`;
+
+export const TextGroup = styled.div`
+  ${Basic}
+  width: 100%;
+`;
+
+export const Title = styled.div`
+  ${ABeePudding}
+  font-size: 3rem;
+`;
+
+export const SubTitle = styled.div`
+  font-size: 1.75rem;
+  color: #828282;
+`;
+
+export const Login = styled.button`
+  font-size: 1.5rem;
+  color: #B2B7B2;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
 `;
