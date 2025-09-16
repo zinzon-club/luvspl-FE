192bfa131e92495400f95561819f73fec9fd4315
soeun823 <bsoeun866@gmail.com>
2025-09-16 09:22:59 +0900

    feat(#5) :: 투두리스트 페이지 구현

---
+++ src/containers/ui/todo/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import * as _ from "./style";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import IsDone from "@/../public/assets/clover/isDone.svg";
+import NotDone from "@/../public/assets/clover/notDone.svg";
+import Image from "next/image";
+
+export default function Todo() {
+  const router = useRouter();
+  const [todo, setTodo] = useState([
+    {
+      value: "일기 쓰기",
+      done: false,
+    },
+    {
+      value: "청소 하기",
+      done: false,
+    },
+    {
+      value: "영단어 외우기",
+      done: true,
+    },
+  ]);
+  return (
+    <_.Container>
+      <_.NavSet>
+        <_.NavDeco onClick={() => router.push(`/mypage`)}>{"<"}</_.NavDeco>
+        <_.TitleSet>
+          <_.Title>오늘의 할일</_.Title>
+          <_.SubTitle>할 일을 누르면 할 일이 완료됩니다</_.SubTitle>
+        </_.TitleSet>
+      </_.NavSet>
+      <_.ResultSet>
+        {todo.map((item, i) => (
+          <_.ResultItem key={i}>
+            <Image
+              src={item.done ? IsDone : NotDone}
+              alt={"todo"}
+              onClick={() => {
+                setTodo(!item.value);
+              }}
+            />
+            <div>{item.value}</div>
+          </_.ResultItem>
+        ))}
+      </_.ResultSet>
+    </_.Container>
+  );
+}
---
+++ src/containers/ui/todo/style.ts
@@ -0,0 +1,60 @@
+import styled from "@emotion/styled";
+import { color } from "@/styles/theme";
+
+const Basic = `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  height: 100vh;
+  margin-top: 20vh;
+  flex-direction: column;
+  gap: 4rem;
+  cursor: default;
+`;
+
+export const NavSet = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  gap: 8rem;
+`;
+export const ResultSet = styled.div`
+  display: flex;
+  //align-items: center;
+  width: 100%;
+  flex-direction: column;
+  gap: 2.5rem;
+`;
+export const ResultItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.25rem;
+  width: 100%;
+  color: ${color.gray600};
+  font-size: 1.25rem;
+`;
+export const NavDeco = styled.div`
+  color: ${color.gray400};
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+export const Title = styled.div`
+  color: ${color.black};
+  font-size: 1.5rem;
+`;
+export const SubTitle = styled.div`
+  color: ${color.gray500};
+  font-size: 1rem;
+`;
+export const TitleSet = styled.div`
+  ${Basic};
+  flex-direction: column;
+`;
