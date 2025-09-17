4e358a4c619eeee1ec12d1541e08db58b9c2fa24
dpqlsn <a01037111209@gmail.com>
2025-09-17 23:10:30 +0900

    feat(#9) :: 좌우 이동 슬라이더 및 애니메이션

---
+++ src/containers/ui/home/style.ts
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding: 0 0.5rem;
+
+  @media screen and (min-width: 440px) {
+    max-width: 430px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const ArrowBtn = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
