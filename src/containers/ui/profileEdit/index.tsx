cffd36318b62bcafbca1de78d3f820aa7cc68bc0
dpqlsn <a01037111209@gmail.com>
2025-11-17 00:47:23 +0900

    perf(#20) :: db profile 사진 들고오기

--- src/containers/ui/profileEdit/index.tsx
+++ src/containers/ui/profileEdit/index.tsx
@@ -22,7 +22,7 @@ const ProfileEdit = () => {
     <_.Container>
       <_.BasicSet>
         <Image
-          src={user.img || "/assets/default.svg"}
+          src={user.profile || "/assets/default.svg"}
           alt="프로필사진"
           width={150}
           height={150}
