d28f63f37445dd122245ec215a33ad2de5e9e542
dpqlsn <a01037111209@gmail.com>
2025-12-08 15:33:49 +0900

    hotfix :: 더미데이터 삭제

--- src/containers/ui/todo/data.ts
+++
@@ -1,7 +0,0 @@
-export default [
-  { id: 1, value: "일기 쓰기", done: false },
-  { id: 2, value: "청소 하기", done: false },
-  { id: 3, value: "영단어 외우기", done: true },
-  { id: 4, value: "집에 가기", done: false },
-  { id: 5, value: "퍼블리싱 하기", done: false },
-];
